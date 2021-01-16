644d4b6713204d825ec8c2ff159b1f120d7a1ffe
Vinicius Reinehr <vinicius.reinehr@gmail.com>
2021-01-16 19:58:04 -0300

    Remove lib from check in tsconfig

--- src/controller/RedirectController.ts
+++ src/controller/RedirectController.ts
@@ -5,25 +5,23 @@ export class RedirectController
 {
     async Main(request: express.Request, response: express.Response)
     {
-        {
-            try {
-                const link: ILink = await Link.findOne({ token: request.params.token });
-        
-                if (!link) {
-                    throw 'URL não localizada!';
-                }
-        
-                const expiration = link.expiration_date;
-                const now = new Date();
-                if (expiration.getTime() < now.getTime()) {
-                    throw `Data de expiração: ${expiration.toLocaleString()} e agora é ${now.toLocaleString()}`;
-                }
-        
-                response.redirect(link.link);
-            } catch (e) {
-                response.status(404);
-                response.send(e);
+        try {
+            const link: ILink = await Link.findOne({ token: request.params.token });
+
+            if (!link) {
+                throw new Error('URL não localizada!');
             }
+
+            const expiration = link.expiration_date;
+            const now = new Date();
+            if (expiration.getTime() < now.getTime()) {
+                throw new Error(`Data de expiração: ${expiration.toLocaleString()} e agora é ${now.toLocaleString()}`);
+            }
+
+            response.redirect(link.link);
+        } catch (e) {
+            response.status(404);
+            response.send(e);
         }
     }
 }
